819a677002b42c643e990bb2ab42e2ae809177b6
Chao Luo <noahgg2012@gmail.com>
2024-05-07 15:56:42 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
+  defaultShowCopyCode: true,
+  // 支持 latex
+  latex: true,
+  // 支持静态图片
+  staticImage: true,
 })
 
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    // next 工程本身配置内容
+    reactStrictMode: true,
+}
 
-
-module.exports = withNextra()
+module.exports = withNextra(nextConfig)
