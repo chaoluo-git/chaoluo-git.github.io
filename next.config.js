286f4bfdefb5101d0b4f22e04f92eb132a8ed333
Chao Luo <noahgg2012@gmail.com>
2024-05-07 16:37:20 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,15 +2,15 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.tsx',
   output: "export",
-})
-
-/**
- defaultShowCopyCode: true,
+  defaultShowCopyCode: true,
   // 支持 latex
   latex: true,
   // 支持静态图片
   staticImage: true,
-*/
+  images: {
+     unoptimized: true
+  }
+})
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
